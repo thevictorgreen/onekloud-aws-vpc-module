a87ba88044501c86ca0110d478823a492cf714ef
Victor Green <thevictorgreen@gmail.com>
2021-11-21 18:56:09 -0500

    initial code

---
+++ outputs.tf
@@ -0,0 +1,69 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = concat(aws_vpc.this.*.id, [""])[0]
+}
+
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = concat(aws_vpc.this.*.arn, [""])[0]
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = concat(aws_vpc.this.*.cidr_block, [""])[0]
+}
+
+output "vpc_owner_id" {
+  description = "The ID of the AWS account that owns the VPC"
+  value       = concat(aws_vpc.this.*.owner_id, [""])[0]
+}
+
+output "private_us_east_2a_sn" {
+  description = "private_us_east_2a_sn"
+  value       = aws_subnet.private[0].id
+}
+
+output "private_us_east_2b_sn" {
+  description = "private_us_east_2b_sn"
+  value       = aws_subnet.private[1].id
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private.*.id
+}
+
+output "private_subnet_arns" {
+  description = "List of ARNs of private subnets"
+  value       = aws_subnet.private.*.arn
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = aws_subnet.private.*.cidr_block
+}
+
+output "public_us_east_2a_sn" {
+  description = "public_us_east_2a_sn"
+  value       = aws_subnet.public[0].id
+}
+
+output "public_us_east_2b_sn" {
+  description = "public_us_east_2b_sn"
+  value       = aws_subnet.public[1].id
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public.*.id
+}
+
+output "public_subnet_arns" {
+  description = "List of ARNs of public subnets"
+  value       = aws_subnet.public.*.arn
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = aws_subnet.public.*.cidr_block
+}
